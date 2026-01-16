eee99ae5f13efb2cbd4176f72060a36382eac76a
Prachi Gupta <prachig150904@gmail.com>
2026-01-17 00:55:50 +0530

    Implement admin and user APIs with order,payment

--- quick-grocer/src/app/models/user.model.ts
+++ quick-grocer/src/app/models/user.model.ts
@@ -1,37 +1,79 @@
-import mongoose from 'mongoose';
-interface IUser {
-  _id?: mongoose.Types.ObjectId;
-  name: string;
-  email: string;
-  password?: string;
-  mobile?: string;
-  role:"user" | "admin" | "deliveryBoy";
-  image?:string
+import mongoose from "mongoose";
+
+export interface IUser{
+    _id?:mongoose.Types.ObjectId
+    name:string
+    email:string
+    password?:string
+    mobile?:string
+    role:"user" | "deliveryBoy" | "admin"
+    image?:string,
+   location?: {
+    type: {
+        type: StringConstructor;
+        enum: string[];
+        default: string;
+    };
+    coordinates: {
+        type: NumberConstructor[];
+        default: number[];
+    };
+},
+socketId:string | null
+isOnline:Boolean
+  
 }
-const userSchema = new mongoose.Schema<IUser>({
-  name: {
-     type: String,
-      required: true 
+
+const userSchema=new mongoose.Schema<IUser>({
+name:{
+    type:String,
+    required:true
+
+},
+email:{
+    type:String,
+    unique:true,
+    required:true
+},
+password:{
+    type:String,
+    required:false
+},
+mobile:{
+    type:String,
+    required:false
+},
+role:{
+    type:String,
+    enum:["user","deliveryBoy","admin"],
+    default:"user"
+},
+image:{
+    type:String
+},
+location:{
+    type:{
+      type:String,
+       enum:["Point"],
+       default:"Point"
     },
-  email: {
-     type: String, 
-     required: true, 
-     unique: true },
-  password: {
-     type: String,
-      required: false 
-    },
-  mobile: {
-     type: String, 
-     required: false 
-    },
-  role: { 
-    type: String,
-     enum: ["user", "deliveryBoy","admin"], default: "user" },
-  image: {
-      type: String,
-      required: false,
-  }
-},{ timestamps: true});
-const User = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
-export default User;
+    coordinates:{
+        type:[Number],
+        default:[0,0]
+    }
+},
+socketId:{
+    type:String,
+    default:null
+},
+isOnline:{
+    type:Boolean,
+    default:false
+}
+
+},{timestamps:true})
+
+userSchema.index({location:"2dsphere"})
+
+const User=mongoose.models.User || mongoose.model("User",userSchema)
+export default User
