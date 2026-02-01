df7d024b93b37af225b07ba6e620e4643e10e251
Pragati4214 <pragatipandey.10.2005@gmail.com>
2026-02-01 16:27:27 +0530

    Added Redux setup: store, StoreProvider, and userSlice

--- quick-grocer/src/app/layout.tsx
+++ quick-grocer/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Provider from "@/provider";
+import StoreProvider from "@/redux/StoreProvider";
 
 
 export const metadata: Metadata = {
@@ -17,7 +18,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className="w-full min-h-screen bg-linear-to-b from-green-400 to-white"
-      ><Provider>{children}</Provider>
+      ><Provider>
+        <StoreProvider>
+          {children}
+        </StoreProvider>
+        </Provider>
         {children}
       </body>
     </html>
