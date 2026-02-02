bce2e74d86ac06a5c1d3d89cc7df70ebddf910be
Pragati4214 <pragatipandey.10.2005@gmail.com>
2026-02-02 23:42:33 +0530

    Created Checkout Page

--- quick-grocer/src/redux/cartSlice.ts
+++ quick-grocer/src/redux/cartSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import mongoose from "mongoose";
 interface IGrocery {
-    _id?: mongoose.Types.ObjectId,
+    _id: mongoose.Types.ObjectId,
     name: string,
     category: string,
     price: string,
@@ -15,11 +15,17 @@ interface IGrocery {
 
 
 interface ICartSlice{
-    cartData: IGrocery[] 
+    cartData: IGrocery[],
+    subTotal?: number,
+    deliveryFee: number,
+    finalTotal: number
 }
 
 const initialState :ICartSlice = {
-    cartData: []
+    cartData: [],
+    subTotal: 0,
+    deliveryFee: 40,
+    finalTotal: 40
 }
 
 const cartSlice = createSlice({
@@ -28,12 +34,14 @@ const cartSlice = createSlice({
     reducers: {
         addToCart: (state, action : PayloadAction<IGrocery>) => {
             state.cartData.push(action.payload)
+            cartSlice.caseReducers.calculateTotals(state)
         },
         increaseQuantity: (state, action : PayloadAction<mongoose.Types.ObjectId>) => {
             const item = state.cartData.find(i => i._id == action.payload)
             if(item) {
                 item.quantity += 1
             }
+            cartSlice.caseReducers.calculateTotals(state)
         },
         decreaseQuantity: (state, action : PayloadAction<mongoose.Types.ObjectId>) => {
             const item = state.cartData.find(i => i._id == action.payload)
@@ -43,9 +51,19 @@ const cartSlice = createSlice({
                 state.cartData = state.cartData.filter(i => i._id != action.payload)    
 
             }
+            cartSlice.caseReducers.calculateTotals(state)
+        },
+        removeFromCart: (state, action : PayloadAction<mongoose.Types.ObjectId>) => {
+            state.cartData = state.cartData.filter(i => i._id !== action.payload)
+            cartSlice.caseReducers.calculateTotals(state)
+        },
+        calculateTotals: (state) => {
+            state.subTotal = state.cartData.reduce((sum, item) => sum + parseFloat(item.price) * item.quantity, 0)
+            state.deliveryFee = state.subTotal >100 ? 0 : 40
+            state.finalTotal = state.subTotal  + state.deliveryFee
         }
     }
 })
 
-export const {addToCart, increaseQuantity, decreaseQuantity} = cartSlice.actions
+export const {addToCart, increaseQuantity, decreaseQuantity, removeFromCart} = cartSlice.actions
 export default cartSlice.reducer
