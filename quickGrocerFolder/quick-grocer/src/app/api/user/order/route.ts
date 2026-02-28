a6a7dd8eb055611ebd9785e0eecee6f3f5e5bcfc
Prachi Gupta <prachig150904@gmail.com>
2026-02-28 14:36:31 +0530

    Add delete-grocery and edit-grocery Admin's API

--- quickGrocerFolder/quick-grocer/src/app/api/user/order/route.ts
+++ quickGrocerFolder/quick-grocer/src/app/api/user/order/route.ts
@@ -1,40 +1,47 @@
 import connectDb from "@/app/lib/db";
+import emitEventHandler from "@/app/lib/emitEventHandler";
 import Order from "@/app/models/order.model";
 import User from "@/app/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  try {
-    await connectDb();
-    const {userId,items,paymentMethod,totalAmount,address}=await req.json();
-    if(!items || !userId || !paymentMethod ||!totalAmount || !address){
-      return NextResponse.json(
-        {message: "All fields are required,send all credentials"},{status: 400}
-      )
-    }
-    const user = await User.findById(userId);
-    if(!user){
-      return NextResponse.json(
-        {message: "User not found"},{status: 400}
-      )
-    }
+    try {
+        await connectDb()
+        const { userId, items, paymentMethod, totalAmount, address } = await req.json()
+        if (!items || !userId || !paymentMethod || !totalAmount || !address) {
+            return NextResponse.json(
+                { message: "please send all credentials" },
+                { status: 400 }
+            )
+        }
+        const user = await User.findById(userId)
+        if (!user) {
+            return NextResponse.json(
+                { message: "user not found" },
+                { status: 400 }
+            )
+        }
 
-   const newOrder = await Order.create({
-    user:user._id,
-    items,
-    paymentMethod,
-    totalAmount,
-    address
-   })
-    return NextResponse.json(
-      {message: "Order placed successfully",order:newOrder},{status: 201}
-    )
+        const newOrder = await Order.create({
+            user: userId,
+            items,
+            paymentMethod,
+            totalAmount,
+            address
+        })
 
-  } catch (error) {
-    return NextResponse.json(
-      {
-      message: "Failed to place order"
+
+        await emitEventHandler("new-order",newOrder)
+
+        return NextResponse.json(
+            newOrder,
+            { status: 201 }
+        )
+
+    } catch (error) {
+ return NextResponse.json(
+                {message:`place order error ${error}`},
+                {status:500}
+            )
     }
-    ,{status: 500})
-  }
 }
